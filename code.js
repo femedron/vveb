b1d13959a379cad2fb16b281288d3f4fae2f3715
AKTuBHbIu_roMeC <succ3917@gmail.com>
2024-05-29 20:50:10 +0300

    Add item name changing

--- code.js
+++ code.js
@@ -11,8 +11,10 @@ function init(){
 
     items_container = document.getElementsByClassName("items-list")[0];
     items_container.addEventListener("click", deleteItem);
+    //remove
     document.querySelectorAll(".delete").forEach(btn => { btn.addEventListener('click', deleteItem);});
 
+    //remove
     document.querySelectorAll('.item').forEach((item)=>{
         item.querySelectorAll('.buy').forEach(btn =>{
             btn.addEventListener('click', ()=>{ changeBuyState(item, btn)});
@@ -47,7 +49,7 @@ function addItem(name = document.getElementById("item-add-text").value, quantity
     child.innerHTML =
         `<span class="item-name">${name}</span>
         <div class="quantity-controls">
-        <button class="btn quantity-btn minus ${quantity == 1 ? 'disabled' : ''}" data-tooltip="-">-
+        <button class="btn quantity-btn minus ${quantity == 1 ? 'disabled" disabled="disabled' : ''}" data-tooltip="-">-
         </button>
         <span class="quantity-value">${quantity}</span>
             <button class="btn quantity-btn plus" data-tooltip="+">+</button>
@@ -58,11 +60,14 @@ function addItem(name = document.getElementById("item-add-text").value, quantity
             </div>`;
     if(isDisabled)
         changeBuyState(child);    
-    list.appendChild(child);
     child.querySelectorAll('.buy').forEach(btn =>{
         btn.addEventListener('click', ()=>{ changeBuyState(child, btn)});
     });
+
+    addNameInputHandler(child);
+        
     items_container.appendChild(child);
+    list.appendChild(child);
 
     if(arguments.length === 0){
         const text_field = document.getElementById("item-add-text");
@@ -71,13 +76,33 @@ function addItem(name = document.getElementById("item-add-text").value, quantity
     }
 }
 
+function addNameInputHandler(item){
+    const span_field = item.querySelector('.item-name');
+    span_field.addEventListener('click', ()=>{
+        let prev_value = span_field.textContent;
+        //<template>
+        const template = document.createElement('template');
+        template.innerHTML = `<input type="text" class="item-name" value="${prev_value}">`;
+        const result = template.content.children;
+
+        let input_field = result[0];
+        span_field.replaceWith(input_field);
+        input_field.focus();
+        input_field.addEventListener('blur', function() {
+                let newValue = input_field.value;
+                span_field.textContent = newValue;
+                input_field.replaceWith(span_field);
+            });
+        });
+}
+
 function changeBuyState(item, btn = item.querySelector(".buy")){
     if(item.classList.contains('bought')){
         item.classList.remove('bought');
         btn.textContent = "Куплено";
 
         const quantity = item.querySelector(".quantity-value").textContent;
-        const minus = `<button class="btn quantity-btn minus ${quantity == 1 ? 'disabled' : ''}" data-tooltip="-">-</button>`;
+        const minus = `<button class="btn quantity-btn minus ${quantity == 1 ? 'disabled" disabled="disabled' : ''}" data-tooltip="-">-</button>`;
         const plus = '<button class="btn quantity-btn plus" data-tooltip="+">+</button>';
         const xxbtn = '<button class="btn action-btn delete" data-tooltip="x">✖</button>';
         item.querySelector(".quantity-controls").insertAdjacentHTML('afterbegin', minus);
